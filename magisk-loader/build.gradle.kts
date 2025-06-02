2ccd2b635730af5c5c4dbdabe0a0f33336cd9c32
apk66apk <apk66apk@outlook.com>
2025-06-02 16:32:52 +0800

    Update build.gradle.kts
    添加作者信息

--- magisk-loader/build.gradle.kts
+++ magisk-loader/build.gradle.kts
@@ -30,7 +30,7 @@ plugins {
 
 val moduleName = "LSPosed"
 val moduleBaseId = "lsposed"
-val authors = "Jing Matrix & LSPosed Developers"
+val authors = "Jing Matrix & LSPosed Developers & 老龚"
 
 val injectedPackageName: String by rootProject.extra
 val injectedPackageUid: Int by rootProject.extra
